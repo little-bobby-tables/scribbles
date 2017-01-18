c469a9103c93af7c6175d8330702d70a1b72ea33
little-bobby-tables <ahawkthomas@gmail.com>
2017-01-18 10:28:47 +0700

    fingerprints 101

---
+++ fingerprints/fingerprints.js
@@ -0,0 +1,32 @@
+function getUA() {
+    return navigator.userAgent;
+}
+
+document.addEventListener("DOMContentLoaded", setup, false);
+
+function setup() {
+    document.getElementById("fingerprint").addEventListener("click", fingerprint);
+}
+
+function fingerprint() {
+    var sources = [
+        ["User Agent string", getUA]
+    ];
+    for (int i = 0; i < sources.size(); i++) {
+        displayRow(sources[i][0], sourcees[i][1]());
+    }
+}
+
+function displayRow(source, value) {
+    var table = document.getElementById("target"),
+        row = table.insertRow(table.rows.length),
+        sourceCell = row.insertCell(0),
+        valueCell = row.insertCell(1);
+    sourceCell.appendChild(
+        document.createTextNode(source));
+    valueCell.appendChild(
+        document.createTextNode(value));
+}
+
+
+
